3596faeda4c142e06f13cb31f7c4113250efdbf3
EminliAysun05 <aeminli232@gmail.com>
2024-05-14 09:30:50 +0400

    feat: add if else case

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@
     {
         static void Main(string[] args)
         {
+            if (20 > 18)
+            {
+                Console.WriteLine("20 is greater than 18");
+            }
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, Aysun!");
             Console.WriteLine("Hello, Aysun!");
